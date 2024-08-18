3bb3e490a1d26a038f694cf0c95a602858893ccb
warren <w.hammock@icloud.com>
2024-08-18 15:16:35 -0400

    inserted new

---
+++ README.md
@@ -0,0 +1,1 @@
+## tutorial from *https://www.digitalocean.com/community/tutorials/how-to-use-flask-sqlalchemy-to-interact-with-databases-in-a-flask-application*
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
-pipenv run flask run -h 127.0.0.1 -p 8000 --debug
+export FLASK_APP=app
+export FLASK_ENV=development
+#pipenv run flask run -h 127.0.0.1 -p 8000 --debug
